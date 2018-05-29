03ab90e06985fd43d4a89dac9cddf0a676a4053f
suitougreentea <suitougreentea@users.noreply.github.com>
2018-05-29 22:56:48 +0900

    Initial commit using electron-vue-typescript-starter

---
+++ src/renderer/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "electron-vue-typescript-starter"
+});
+</script>
+
+<style>
+/* CSS */
+</style>
