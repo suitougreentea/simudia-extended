b863a80dc8224ceae94715dc1ff2db93d0367642
suitougreentea <suitougreentea@users.noreply.github.com>
2023-11-06 23:01:17 +0900

    Install Vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+// import 'vuetify/styles'
 import MainScreen from './main/MainScreen.vue'
 
 const app = createApp(MainScreen)
 
 app.use(createPinia())
+app.use(createVuetify({ components, directives }))
 
 app.mount('#app')
