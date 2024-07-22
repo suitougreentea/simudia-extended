4b304327c9bb395aab73d59cb54c27c0e5d8316c
suitougreentea <suitougreentea@users.noreply.github.com>
2024-07-22 10:20:43 +0900

    add (immutable) StationNames component

---
+++ src/main/StationNames.vue
@@ -0,0 +1,24 @@
+<template>
+  <text
+    v-for="s in gui.stations"
+    ref="existingStation"
+    type="text"
+    class="station-name"
+    x="20"
+    :y="gui.y(s.accumulatedTime) - 6">
+    {{ s.name }}
+  </text>
+
+</template>
+
+<script setup lang="ts">
+import { useGuiStore } from "../stores/gui"
+
+const gui = useGuiStore()
+</script>
+
+<style scoped>
+.station-name {
+  font-size: 14px;
+}
+</style>
